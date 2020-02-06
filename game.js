8001c45b7d617a52d66c5bf3387222cbdcd0bcb9
maxmisling <60746519+maxmisling@users.noreply.github.com>
2020-02-06 17:55:05 +0000

    Create game.js

---
+++ game.js
@@ -0,0 +1,19 @@
+function addRandomSmiley() {
+  $('div').removeClass();
+  var randomIndex = Math.floor(Math.random() * 9);
+  $('div').eq(randomIndex).addClass('smiley');
+}
+
+function handleClick() {
+
+  if ($(this).hasClass('smiley')) {
+    $(this).removeClass('smiley').addClass('win');
+  }
+
+}
+
+$('div').click(handleClick);
+
+setInterval(addRandomSmiley, 2000);
+
+addRandomSmiley();
